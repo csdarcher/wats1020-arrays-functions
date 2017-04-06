fa990af4482273f616db5fb9bffb5606d1d73855
casdarcher <casdarcher@gmail.com>
2017-04-06 14:06:42 -0400

    Commited JS file for review

--- truncate.js
+++ truncate.js
@@ -3,15 +3,29 @@
 // TODO: Create a Function called truncateWords() that accepts two arguments: 
 // longText (a String with several words in it)
 // numWords (an Integer that sets the number of words you want in the returned text)
+var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
+function truncateWords() {
 
-// TODO: Within the truncateWords() Function, complete the following steps:
-// 1. Use the split() function to split the String into an Array
-// 2. Use the length attribute to find the number of words in the Array
-// 3. Determine how many words should be removed from the String
-// 4. Remove those words from the Array
-// 5. Add an additional String item to the Array to put an ellipses in: "..."
-// 6. Use the join() function to convert the Array back into a String
-// 7. Return the truncated String from the Function
+// Use the split() function to split the String into an Array   
+  var arrayOfStrings = originalText.split(" ");
+
+  
+// Use the length attribute to find the number of words in the Array  
+  var totalLength = arrayOfStrings.length;
+
+  
+// Determine how many words should be removed from the String. Use wordLimit() and then Use splice() to remove those words from the Array
+// Add an additional String item to the Array to put an ellipses in: "..."
+  var wordLimit = arrayOfStrings.splice(8);
+
+
+// Use the join() function to convert the Array back into a String
+  var newString = wordLimit.join(" ");  
+
+// Return the truncated String from the Function
+  return newString;
+  
+}
 
 // This portion of the script is meant to call and display the result of your Function.
 // You do not need to change the following lines, but you may change them if you 
